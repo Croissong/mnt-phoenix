aa9fae01b1dd26883e9310d9383054bfbaa9a794
Jan Moeller <jan.moeller0@gmail.com>
2016-12-11 06:21:30 +0100

    (Blog) Add websocket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule Blog.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Blog.RoomChannel
+  channel "room:*", Blog.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
